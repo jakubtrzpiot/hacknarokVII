2faea255b8fc364030a8b0ddbee4584b3aae3227
PatrykNizio <patryknizio@interia.pl>
2023-04-02 07:31:38 +0200

    Challanges and Profile. Lord of the rings

--- src/screens/Challenges.jsx
+++ src/screens/Challenges.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/dist/EvilIcons';
 
 const Challenges = () => {
   return (
-    <View>
-      <Text>Challenges</Text>
+    <View className="flex-col items-center justify-center my-10">
+      <Text className="text-4xl">Daily Challanges</Text>
+      <Icon name="trophy" size={400} color="#900" />
     </View>
   );
 };
--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,10 +1,76 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/dist/EvilIcons';
 
 const Profile = () => {
+  const achievements = [
+    {
+      category: 'Daily Challenges',
+      title: 'Fulfill 20 daily challenges.',
+      icon: 'trophy',
+    },
+    {
+      category: 'Daily Challenges',
+      title: 'Get 10 day hot streak.',
+      icon: 'trophy',
+    },
+    { category: 'Task Management', title: 'Pin a task.', icon: 'paperclip' },
+    {
+      category: 'Task Management',
+      title: 'Bring to fruition your first task',
+      icon: 'paperclip',
+    },
+    {
+      category: 'Daily Challenges',
+      title: 'Fulfill 10 daily challenges',
+      icon: 'trophy',
+    },
+    {
+      category: 'Productivity Streaks',
+      title: 'Complete 5 tasks in a day',
+      icon: 'gear',
+    },
+    {
+      category: 'Productivity Streaks',
+      title: 'Complete 10 tasks in a day',
+      icon: 'gear',
+    },
+    {
+      category: 'Productivity Streaks',
+      title: 'Complete 20 tasks in a day',
+      icon: 'gear',
+    },
+    {
+      category: 'Task Management',
+      title: 'Create a new category',
+      icon: 'paperclip',
+    },
+    {
+      category: 'Task Management',
+      title: 'Add a due date to a task',
+      icon: 'paperclip',
+    },
+  ];
+
   return (
-    <View>
-      <Text>Profile</Text>
+    <View className="flex-col items-center justify-center my-10">
+      <View>
+        <Text className="text-3xl">Hot Streak</Text>
+      </View>
+      <View className="border-y-0 border-black w-screen items-center p-10">
+        <Text className="text-3xl">Achievement</Text>
+      </View>
+      <ScrollView>
+        {achievements.map((item, index) => (
+          <View
+            key={index}
+            className="flex-row border-y-0 border-black w-screen items-center p-5 m-2"
+          >
+            <Icon name={item.icon} size={70} />
+            <Text className="it">{item.title}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 };
